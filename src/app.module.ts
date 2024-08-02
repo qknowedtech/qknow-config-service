8edc7929d8f2102b1d0e828f3fe53d2d2583a833
Reginald Addo <addoreginald360@gmail.com>
2024-08-02 15:13:14 +0000

    removed authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,16 @@
-
-import { MiddlewareConsumer, Module, NestModule, Logger, RequestMethod } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import {
+  Module
+} from '@nestjs/common';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HttpModule } from '@nestjs/axios';
-import * as dotenv from 'dotenv';
-import { BasicAuthValidatorMiddleware } from './middlewares/basic-auth-validator/basic-auth-validator.middleware';
 
 dotenv.config();
 
 @Module({
-
   imports: [HttpModule.register({ timeout: 5000, maxRedirects: 5 })],
-
   controllers: [AppController],
-
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-
-  configure(consumer: MiddlewareConsumer) {
-
-    consumer.apply(BasicAuthValidatorMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL });
-  }
-  constructor() {
-    Logger.log(`PORT: ${process.env.PORT}`, 'AppModule');
-  }
-}
+export class AppModule {}
